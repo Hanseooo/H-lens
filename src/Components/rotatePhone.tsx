631a6727a9a460be9a2b5e369d1891383d21a7cb
Hanseooo <amoguishans@gmail.com>
2025-02-23 10:09:05 +0800

    added an extra paragraph on the rotatephone component

--- src/Components/rotatePhone.tsx
+++ src/Components/rotatePhone.tsx
@@ -8,6 +8,7 @@ export default function RotatePhone() {
                 <img className='rotatePhone' src={rotatePhone} style={{width: "300px"}}></img>
                 <h2 className='scale-in-ver-top text-light fw-bold fs-1' style={{animationDelay: "0.25s"}}>Rotate Phone</h2>
                 <h3 className='scale-in-ver-top text-light text-center p-2' style={{animationDelay: "0.45s"}}>For Mobile users, please use<span className='fw-bold text-decoration-underline'> landscape</span> mode for better image quality</h3>
+                <p className='scale-in-ver-top text-light text-center p-2 fw-light m-2' style={{animationDelay: "0.75s"}}>enable auto-rotate if necessary</p>
          </div>
     )
 }
